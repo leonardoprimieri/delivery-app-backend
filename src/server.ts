ee52907ab30c6f6ebd660190ae2870d8c268560e
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-10-27 17:14:07 -0300

    feat: add routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { json } from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(json);
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running!"));
